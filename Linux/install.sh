28b062f74a8af5241911ddb4304acec5e8784ced
Sivan Monshi <sivan.monshi@gmail.com>
2025-04-28 23:24:39 +0300

    update toolz

---
+++ Linux/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Installing Toolz..."
+
+sudo cp toolz /usr/local/bin/toolz
+sudo chmod +x /usr/local/bin/toolz
+
+echo "Toolz has been installed successfully!"
+echo "You can now run it from anywhere by typing: toolz"
